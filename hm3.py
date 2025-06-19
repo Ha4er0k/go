3e707d4973d0a2be4354792e54ba6ab120c5a111
Ha4er0k <dimasymy2017@gmail.com>
2025-06-19 23:18:39 +0200

    goit-algo-hw-06

--- hm3.py
+++ hm3.py
@@ -32,9 +32,6 @@ for stations in lines.values():
 shortest_paths = dict(nx.all_pairs_dijkstra_path_length(G, weight='weight'))
 
 #приклад: час від Isolatorweg до всіх станцій
-print("⏱ Найкоротший час від 'Isolatorweg' до інших станцій (в хвилинах):\n")
+print("Найкоротший час від 'Isolatorweg' до інших станцій (в хвилинах):\n")
 for station, time in sorted(shortest_paths["Isolatorweg"].items(), key=lambda x: x[1]):
     print(f"{station}: {time} хв")
-
-
-
