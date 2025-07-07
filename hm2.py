68f0d88719e37e61740b76e06293f68d6241d5db
Ha4er0k <dimasymy2017@gmail.com>
2025-07-07 20:26:12 +0200

    goit-cs-hw-01

--- hm2.py
+++ hm2.py
@@ -1,43 +1,193 @@
-import turtle
-import math
+class LexicalError(Exception):
+    pass
 
-def draw_pythagoras_tree(t, branch_length, level, angle=45):
-    if level == 0:
-        return
-    t.forward(branch_length)
-    x, y = t.position()
-    heading = t.heading()
-    t.left(angle)
-    draw_pythagoras_tree(t, branch_length * math.cos(math.radians(angle)), level - 1, angle)
-    t.penup()
-    t.setposition(x, y)
-    t.setheading(heading)
-    t.pendown()
-    t.right(angle)
-    draw_pythagoras_tree(t, branch_length * math.sin(math.radians(angle)), level - 1, angle)
-    t.penup()
-    t.setposition(x, y)
-    t.setheading(heading)
-    t.pendown()
+class ParsingError(Exception):
+    pass
+
+class TokenType:
+    INTEGER = "INTEGER"
+    PLUS = "PLUS"
+    MINUS = "MINUS"
+    MUL = "MUL"
+    DIV = "DIV"
+    LPAREN = "LPAREN"
+    RPAREN = "RPAREN"
+    EOF = "EOF"
+
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        return f"Token({self.type}, {repr(self.value)})"
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_char = self.text[self.pos]
+
+    def advance(self):
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        result = ""
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+    def get_next_token(self):
+        while self.current_char is not None:
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.current_char.isdigit():
+                return Token(TokenType.INTEGER, self.integer())
+
+            if self.current_char == "+":
+                self.advance()
+                return Token(TokenType.PLUS, "+")
+            if self.current_char == "-":
+                self.advance()
+                return Token(TokenType.MINUS, "-")
+            if self.current_char == "*":
+                self.advance()
+                return Token(TokenType.MUL, "*")
+            if self.current_char == "/":
+                self.advance()
+                return Token(TokenType.DIV, "/")
+            if self.current_char == "(":
+                self.advance()
+                return Token(TokenType.LPAREN, "(")
+            if self.current_char == ")":
+                self.advance()
+                return Token(TokenType.RPAREN, ")")
+
+            raise LexicalError("Помилка лексичного аналізу")
+
+        return Token(TokenType.EOF, None)
+
+class AST:
+    pass
+
+class BinOp(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.op = op
+        self.right = right
+
+class Num(AST):
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+class Parser:
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
+
+    def error(self):
+        raise ParsingError("Помилка синтаксичного аналізу")
+
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error()
+
+    def factor(self):
+        token = self.current_token
+        if token.type == TokenType.INTEGER:
+            self.eat(TokenType.INTEGER)
+            return Num(token)
+        elif token.type == TokenType.LPAREN:
+            self.eat(TokenType.LPAREN)
+            node = self.expr()
+            self.eat(TokenType.RPAREN)
+            return node
+        else:
+            self.error()
+
+    def term(self):
+        node = self.factor()
+
+        while self.current_token.type in (TokenType.MUL, TokenType.DIV):
+            token = self.current_token
+            if token.type == TokenType.MUL:
+                self.eat(TokenType.MUL)
+            elif token.type == TokenType.DIV:
+                self.eat(TokenType.DIV)
+            node = BinOp(left=node, op=token, right=self.factor())
+
+        return node
+
+    def expr(self):
+        node = self.term()
+
+        while self.current_token.type in (TokenType.PLUS, TokenType.MINUS):
+            token = self.current_token
+            if token.type == TokenType.PLUS:
+                self.eat(TokenType.PLUS)
+            elif token.type == TokenType.MINUS:
+                self.eat(TokenType.MINUS)
+            node = BinOp(left=node, op=token, right=self.term())
+
+        return node
+
+class Interpreter:
+    def __init__(self, parser):
+        self.parser = parser
+
+    def visit_BinOp(self, node):
+        if node.op.type == TokenType.PLUS:
+            return self.visit(node.left) + self.visit(node.right)
+        elif node.op.type == TokenType.MINUS:
+            return self.visit(node.left) - self.visit(node.right)
+        elif node.op.type == TokenType.MUL:
+            return self.visit(node.left) * self.visit(node.right)
+        elif node.op.type == TokenType.DIV:
+            return self.visit(node.left) / self.visit(node.right)
+
+    def visit_Num(self, node):
+        return node.value
+
+    def visit(self, node):
+        method_name = "visit_" + type(node).__name__
+        visitor = getattr(self, method_name, self.generic_visit)
+        return visitor(node)
+
+    def generic_visit(self, node):
+        raise Exception(f"Немає методу visit_{type(node).__name__}")
+
+    def interpret(self):
+        tree = self.parser.expr()
+        return self.visit(tree)
 
 def main():
-    level = int(input("Введіть рівень рекурсії (наприклад, 6): "))
-
-    screen = turtle.Screen()
-    screen.bgcolor("white")
-    t = turtle.Turtle()
-    t.color("brown")
-    t.speed(0)
-    t.left(90)
-
-    t.penup()
-    t.goto(0, -250)
-    t.pendown()
-
-    draw_pythagoras_tree(t, 100, level)
-    t.hideturtle()
-    screen.exitonclick()
+    while True:
+        try:
+            text = input('Введіть вираз (або "exit" для виходу): ')
+            if text.lower() == "exit":
+                print("Вихід із програми.")
+                break
+            lexer = Lexer(text)
+            parser = Parser(lexer)
+            interpreter = Interpreter(parser)
+            result = interpreter.interpret()
+            print(result)
+        except Exception as e:
+            print("Помилка:", e)
 
 if __name__ == "__main__":
     main()
-
